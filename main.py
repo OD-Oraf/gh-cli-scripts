249083c4445b2ac210db740ebf36fb8c12e08dd9
odoraf <poseidonog@gmail.com>
2025-09-03 21:40:18 -0400

    update main file to with import statements from gh_api_client.py

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ Main execution script for GitHub App Repository Management
 from dotenv import load_dotenv
 import os
 
-from gh_script import list_user_repos
-from gh_script import add_repo_to_app_installation
+from gh_api_client import list_user_repos
+from gh_api_client import add_repo_to_app_installation
 from gen_app_token import get_github_app_token
-from gh_script import authenticate_with_github_app_token
-import gh_script
+from gh_api_client import authenticate_with_github_app_token
+import gh_api_client
 
 
 import gen_app_token as generate_github_token
@@ -27,14 +27,12 @@ def main():
     """Main execution function"""
     print("🚀 GitHub App Repository Management with GitHub CLI")
     log_banner()
-    # Step 1: List your repositories
+
+    # Step 1: Authenticate with PAT to get List of repositories
     print("🚀 Listing GitHub Repositories")
-    gh_script.authenticate_with_pat(os.getenv("GITHUB_PAT"))
+    gh_api_client.authenticate_with_pat(os.getenv("GITHUB_PAT"))
 
-    # Step 1 Use GitHub App Authentication Token
-
-    
-    # Step 1: List your repositories
+    # Step 1: Create list of repository IDs
     print("🚀 Listing GitHub Repositories")
     repo_ids = []
     repo_list = list_user_repos()
@@ -42,15 +40,15 @@ def main():
         repo_ids.append(repo["id"])
     print("repo_ids: ", repo_ids)
 
-    print("🚀 Authenticating with GitHub App Token")
-    github_app_token = get_github_app_token()
-    print("github_app_token: ", github_app_token)
-    authenticate_with_github_app_token(github_app_token)
-    
-    # Step 1: List your repositories
-    print("🚀 Listing GitHub Repositories")
-    list_user_repos()
-    
+    # Get GitHub App Token for adding repositories to the installation
+    # print("🚀 Getting GitHub App Token")
+    # github_app_token = get_github_app_token()
+    # print("github_app_token: ", github_app_token)
+
+    # Add repositories to GitHub App installation
+    # for repo_id in repo_ids:
+    #     add_repo_to_app_installation(os.getenv("GITHUB_APP_INSTALLATION_ID"), repo_id, github_app_token)
+    #
     print("\n" + "=" * 60)
 
 
